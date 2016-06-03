3db328a406c452991d226e5411be6ff48502d013
Pzzul <lsc817ee@gmail.com>
2016-06-03 12:26:19 -0400

    add single_test method in test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from scheduler import Scheduler
 import logging
+import urllib2
 
 
 def test():
@@ -11,5 +12,20 @@ def test():
     sd = Scheduler(**schedule_config)
     sd.run()
 
-test()
 
+def single_test(avvo_id):
+    user_agent = '''Mozilla/5.0 (Windows NT 10.0; WOW64)
+        AppleWebKit/537.36 (KHTML, like Gecko)
+        Chrome/50.0.2661.102 Safari/537.36'''
+    base_url = "https://www.avvo.com/attorneys/"
+    url = base_url + str(avvo_id) + ".html"
+    request = urllib2.Request(url)
+    request.add_header('User-Agent', user_agent)
+    from demo import LawyerInfo
+    lawyer_info = LawyerInfo(request, avvo_id)
+    lawyer = lawyer_info.parse()
+    print lawyer
+
+
+#test()
+single_test(7524)
